00fa988f413361c7e5cae14e6f13c5b8a256d7e6
Renan Barbosa <renan.vinicius@sga.pucminas.br>
2020-08-18 09:02:52 -0300

    Implementação da controller de produto

---
+++ src/Estoque.Web.Api/Controllers/ProductsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Estoque.Web.Api.Core.Domain.Entities;
+using Estoque.Web.Api.Infra;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Estoque.Web.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            var product = await _productRepository.Get(id);
+            return Ok(product);
+        }
+        [HttpPost]
+        public async Task<ActionResult> Add(Product entity)
+        {
+            await _productRepository.Insert(entity);
+            return Ok(entity);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Product>> Update(Product entity)
+        {
+            await _productRepository.Update(entity);
+            return Ok(entity);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _productRepository.Delete(id);
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Product>> GetAll()
+        {
+            var products = await _productRepository.GetAll();
+            return Ok(products);
+        }
+    }
+}
